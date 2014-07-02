8132cb4d8bd5de37b93b13b1a371b4a8004522a7
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2014-07-01 23:31:59 -0400

    update examples and go to 0.1.0

--- examples/schema.js
+++ examples/schema.js
@@ -6,13 +6,12 @@ var client,
 client = window.mongoscope({scope: 'http://scope.mongodb.land'}).on('readable', example);
 
 function example(){
-  client.sample('canada', 'service_requests', {size: 20}, function(err, res){
+  client.sample('canada.service_requests', {size: 20}, function(err, res){
     $('.raw').text(JSON.stringify(res, null, 2));
 
     var schemas = res.map(function(doc){
       return infer(doc);
     });
     $('.schema-raw').text(JSON.stringify(schemas, null, 2));
-
   });
 }
